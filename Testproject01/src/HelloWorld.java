3c15d0aec25d92895635bec9bf2e549643e5eafb
takanoko <c.azurin21@gmail.com>
2019-03-25 20:06:41 +0900

    #####

---
+++ Testproject01/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello World! の出力
+		System.out.println("Hello World!");
+		String str1="鈴木";
+		System.out.println(str1+"さん");
+
+	}
+
+}
