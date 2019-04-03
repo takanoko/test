9f80ef51890ce61fa3a2daf3365591d066ef3872
internousdevwork <internous.dev@gmail.com>
2019-04-03 21:36:48 +0900

    43

--- TestDB/sql/sampledb.sql
+++ TestDB/sql/sampledb.sql
@@ -3,10 +3,10 @@ create database sampledb;
 use sampledb;
 
 create table sample_table(
-user_id int,
-user_name varchar(50),
-user_age int,
-user_mail varchar(50));
+No int,
+name varchar(50),
+age int,
+Mail varchar(50));
 
 insert into sample_table values(1,"Yamada",21,"xxx@yahoo.co.jp");
 insert into sample_table values(2,"Sato",39,"yyy@google.com");
